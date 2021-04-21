0ad81963adbc62ecfeb7bfd1a69c7ebebb3726e8
jiashuang <jias.tw@gmail.com>
2021-04-21 09:03:12 +0800

    tile-fusion: update

--- tile-fusion.py
+++ tile-fusion.py
@@ -15,44 +15,47 @@ class Pad:
         self.top = top
         self.bottom = bottom
     def parse_desc(self, desc):
-        vals = list(map(int, re.split('_|x', desc)))
-        if len(vals) >= 2:
-            self.left = vals[0]
-            self.right = vals[1]
-        if len(vals) >= 4:
-            self.top = vals[2]
-            self.bottom = vals[3]
+        m = re.search(r'pad=(\w+)', desc)
+        if m:
+            vals = list(map(int, re.split('_|x', m.group(1))))
+            if len(vals) >= 2:
+                self.left = vals[0]
+                self.right = vals[1]
+            if len(vals) >= 4:
+                self.top = vals[2]
+                self.bottom = vals[3]
 
 class Flt:
-    def __init__(self, width, height, stride_x = 1, stride_y = 1, dilation_x = 1, dilation_y = 1):
-        self.width = width
-        self.height = height
-        self.stride_x = stride_x
-        self.stride_y = stride_y
-        self.dilation_x = dilation_x
-        self.dilation_y = dilation_y
+    def __init__(self, w, h, sx = 1, sy = 1, dx = 1, dy = 1):
+        self.w = w
+        self.h = h
+        self.sx = sx
+        self.sy = sy
+        self.dx = dx
+        self.dy = dy
     def parse_desc(self, desc):
-        vals = list(map(int, re.split('_|x', desc)))
-        if len(vals) >= 2:
-            self.width = vals[0]
-            self.height = vals[1]
-        if len(vals) >= 4:
-            self.stride_x = vals[2]
-            self.stride_y = vals[3]
-        if len(vals) >= 6:
-            self.dilation_x = vals[4]
-            self.dilation_y = vals[5]
+        m = re.search(r'flt=(\d+)x(\d+)', desc)
+        if m:
+            self.w, self.h = int(m.group(1)), int(m.group(2))
+        m = re.search(r'stride=(\d+)x(\d+)', desc)
+        if m:
+            self.sx, self.sy = int(m.group(1)), int(m.group(2))
+        m = re.search(r'dilation=(\d+)x(\d+)', desc)
+        if m:
+            self.dx, self.dy = int(m.group(1)), int(m.group(2))
 
 class Layer:
-    def __init__(self, width = 0, height = 0, flt = None, pad = None, pad_type = None):
-        self.width = width
-        self.height = height
+    def __init__(self, w = 0, h = 0, out_w = 0, out_h = 0, flt = None, pad = None, pad_type = None):
+        self.w = w
+        self.w = h
+        self.out_w = out_w
+        self.out_h = out_h
         self.flt = flt or Flt(1, 1, 1, 1)
         self.pad = pad or Pad(0, 0, 0, 0)
         self.pad_type = pad_type
-    def set_pad_by_fo(self, out_w, out_h):
-        pad_w = (out_w - 1) * self.flt.stride_x + self.flt.width + (self.flt.width - 1) * (self.flt.dilation_x - 1) - self.width
-        pad_h = (out_h - 1) * self.flt.stride_y + self.flt.height + (self.flt.height - 1) * (self.flt.dilation_y - 1) - self.height
+    def set_pad_by_fo(self):
+        pad_w = (self.out_w - 1) * self.flt.sx + self.flt.w + (self.flt.w - 1) * (self.flt.dx - 1) - self.w
+        pad_h = (self.out_h - 1) * self.flt.sy + self.flt.h + (self.flt.h - 1) * (self.flt.dy - 1) - self.h
         left = pad_w // 2
         right = pad_w - left
         top = pad_h // 2
@@ -61,59 +64,58 @@ class Layer:
     def get_tiles(self, tile_w, tile_h):
         tiles = []
         y = 0
-        while y < self.height:
+        while y < self.h:
             x = 0
-            while x < self.width:
-                w = min(tile_w, self.width - x)
-                h = min(tile_h, self.height - y)
+            while x < self.w:
+                w = min(tile_w, self.w - x)
+                h = min(tile_h, self.h - y)
                 tiles.append(Tile(x, y, w, h))
                 x += w
             y += tile_h
         return tiles
     def get_tile_by_output(self, out_tile):
         # w/ paddings
-        x = out_tile.x * self.flt.stride_x
-        y = out_tile.y * self.flt.stride_y
-        w = (out_tile.w - 1) * self.flt.stride_x + self.flt.width + (self.flt.width - 1) * (self.flt.dilation_x - 1)
-        h = (out_tile.h - 1) * self.flt.stride_y + self.flt.height + (self.flt.height - 1) * (self.flt.dilation_y - 1)
+        x = out_tile.x * self.flt.sx
+        y = out_tile.y * self.flt.sy
+        w = (out_tile.w - 1) * self.flt.sx + self.flt.w + (self.flt.w - 1) * (self.flt.dx - 1)
+        h = (out_tile.h - 1) * self.flt.sy + self.flt.h + (self.flt.h - 1) * (self.flt.dy - 1)
         # w/o paddings
         if x < self.pad.left:
-            w = max(min(x + w - self.pad.left, self.width), 0)
+            w = max(min(x + w - self.pad.left, self.w), 0)
             x = 0
-        elif x in range(self.pad.left, self.pad.left + self.width + 1):
-            w = min(w, self.pad.left + self.width - x)
+        elif x in range(self.pad.left, self.pad.left + self.w + 1):
+            w = min(w, self.pad.left + self.w - x)
             x -= self.pad.left
         else:
             w = 0
-            x = self.width
+            x = self.w
         if y < self.pad.top:
-            h = max(min(y + h - self.pad.top, self.height), 0)
+            h = max(min(y + h - self.pad.top, self.h), 0)
             y = 0
-        elif y in range(self.pad.top, self.pad.top + self.height + 1):
-            h = min(h, self.pad.top + self.height - y)
+        elif y in range(self.pad.top, self.pad.top + self.h + 1):
+            h = min(h, self.pad.top + self.h - y)
             y -= self.pad.top
         else:
             h = 0
-            y = self.height
+            y = self.h
         return Tile(x, y, w, h)
     def parse_desc(self, desc):
-        m = re.search('dim=(\w+)', desc)
+        m = re.search('in=(\d+)x(\d+)', desc)
         if m:
-            vals = list(map(int, re.split('_|x', m.group(1))))
-            self.width = vals[0]
-            self.height = vals[1]
+            self.w, self.h = int(m.group(1)), int(m.group(2))
+        m = re.search('out=(\d+)x(\d+)', desc)
+        if m:
+            self.out_w, self.out_h = int(m.group(1)), int(m.group(2))
         m = re.search('pad_type=(\w+)', desc)
         if m:
             self.pad_type = m.group(1)
-        m = re.search('flt=(\w+)', desc)
-        if m:
-            self.flt.parse_desc(m.group(1))
-        m = re.search('pad=(\w+)', desc)
-        if m:
-            self.pad.parse_desc(m.group(1))
+        self.flt.parse_desc(desc)
+        self.pad.parse_desc(desc)
+        if self.pad_type == 'SAME':
+            self.set_pad_by_fo()
     def get_dim_with_padding(self):
-        w = self.width + self.pad.left + self.pad.right
-        h = self.height + self.pad.top + self.pad.bottom
+        w = self.w + self.pad.left + self.pad.right
+        h = self.h + self.pad.top + self.pad.bottom
         return (w, h)
 
 class Tile:
@@ -169,11 +171,11 @@ def do_plot(layers, tiles_by_layer):
         ax.add_patch(Rectangle((x, y), w, h, color = 'black', fill=False))
         # w/o padding
         pos = (x + layer.pad.left, y + layer.pad.top)
-        ax.add_patch(Rectangle(pos, layer.width, layer.height, color='blue', fill=False))
-        plt.text(x, -10, 'layer {}x{} (pad: {}x{})\nflt {}x{} stride {}x{} dilation {}x{}'.format(layer.width, layer.height, w, h,
-            layer.flt.width, layer.flt.height,
-            layer.flt.stride_x, layer.flt.stride_y,
-            layer.flt.dilation_x, layer.flt.dilation_y))
+        ax.add_patch(Rectangle(pos, layer.w, layer.h, color='blue', fill=False))
+        plt.text(x, -10, 'layer {}x{} (pad: {}x{})\nflt {}x{} stride {}x{} dilation {}x{}'.format(layer.w, layer.h, w, h,
+            layer.flt.w, layer.flt.h,
+            layer.flt.sx, layer.flt.sy,
+            layer.flt.dx, layer.flt.dy))
         for tile_idx, tile in enumerate(tiles_by_layer[idx]):
             if tile.w and tile.h:
                 pos = (x + layer.pad.left + tile.x, y + layer.pad.top + tile.y)
@@ -218,19 +220,13 @@ def main():
         layers.append(Layer())
         layers[-1].parse_desc(layer_desc)
 
-    # update padding from next layer
-    for idx, layer in enumerate(layers[:-1]):
-        if isinstance(layer.pad_type, str):
-            if layer.pad_type == 'SAME':
-                layer.set_pad_by_fo(layers[idx+1].width, layers[idx+1].height)
-
     print('-' * 80)
     for idx, layer in enumerate(layers):
-        print('layer {} orig {}x{} padding ({}, {}, {}, {}) => {}x{}'.format(
-            idx, layer.width, layer.height,
+        print('layer {} : in {}x{} pad {}x{} ({}, {}, {}, {}) out {}x{}'.format(
+            idx, layer.w, layer.h,
+            layer.w + layer.pad.left + layer.pad.right, layer.h + layer.pad.top + layer.pad.bottom,
             layer.pad.left, layer.pad.right, layer.pad.top, layer.pad.bottom,
-            layer.width+layer.pad.left+layer.pad.right,
-            layer.height+layer.pad.top+layer.pad.bottom))
+            layer.out_w, layer.out_h))
 
     tile_dim = list(map(int, args.tile_dim.split('x')))
     tiles_by_layer = do_tile_fusion(layers, tile_dim[0], tile_dim[1])
