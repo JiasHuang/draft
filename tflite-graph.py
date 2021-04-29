2328e5e8c3e5a2ab25564cc8594d9d9bc3314b60
jiashuang <jias.tw@gmail.com>
2021-04-29 21:40:37 +0800

    refine tflite-graph

--- tflite-graph.py
+++ tflite-graph.py
@@ -6,24 +6,106 @@ import configparser
 import argparse
 import tflite
 import numpy
-
-from graphviz import Digraph
+import graphviz
 
 class defvals:
     section = 'tflite-graph'
 
-class OpCtx:
+class OpInfo:
     def __init__(self, idx, op_code):
         self.idx = idx
-        self.op_code = op_code.BuiltinCode()
+        self.op_code = op_code
         self.op_name = tflite.opcode2name(self.op_code)
         self.output = None
         self.succ = []
         self.fus_grp = []
+
     def __str__(self):
         return '%d_%s' %(self.idx, self.op_name)
+
     def nodename(self):
         return '%d_%s' %(self.idx, self.op_name)
+
+class ModelParser:
+    def __init__(self, args):
+        self.args = args
+        self.model = None
+        self.subgraph = None
+        self.ops = []
+
+    def __str__(self):
+        return '\n'.join([op.nodename() for op in self.ops])
+
+    def load_model(self):
+        with open(self.args.model, 'rb') as fd:
+            self.model = tflite.Model.GetRootAsModel(fd.read(), 0)
+        return self.model
+
+    def get_successor(self, op_idx):
+        results = []
+        op = self.subgraph.Operators(op_idx)
+        outputs = op.OutputsAsNumpy()
+        for i in range(self.subgraph.OperatorsLength()):
+            op = self.subgraph.Operators(i)
+            for x in op.InputsAsNumpy():
+                if x in outputs:
+                    results.append(i)
+        return results
+
+    def get_input(self, op_idx, i):
+        op = self.subgraph.Operators(op_idx)
+        if i in range(op.InputsLength()):
+            tensor_index = op.Inputs(i)
+            tensor = self.subgraph.Tensors(tensor_index)
+            return tensor.ShapeAsNumpy()
+        return None
+
+    def get_output(self, op_idx, i):
+        op = self.subgraph.Operators(op_idx)
+        if i in range(op.OutputsLength()):
+            tensor_index = op.Outputs(i)
+            tensor = self.subgraph.Tensors(tensor_index)
+            return tensor.ShapeAsNumpy()
+        return None
+
+    def parse_subgraph(self):
+        self.subgraph = self.model.Subgraphs(0)
+        for op_idx in range(self.subgraph.OperatorsLength()):
+            op = self.subgraph.Operators(op_idx)
+            info = OpInfo(op_idx, self.model.OperatorCodes(op.OpcodeIndex()).BuiltinCode())
+            info.succ = self.get_successor(op_idx)
+            info.output = self.get_output(op_idx, 0)
+            self.ops.append(info)
+        return self.ops
+
+    def plot(self):
+        g = graphviz.Digraph()
+        for op in self.ops:
+            if len(op.fus_grp) > 1:
+                with g.subgraph(name='cluster_'+str(op.fus_grp)) as c:
+                    c.attr(color='blue')
+                    c.attr(label=str(op.fus_grp))
+                    c.edge_attr['style'] = 'invis'
+                    for x in range(len(op.fus_grp) - 1):
+                        c.edge(str(op.fus_grp[x]), str(op.fus_grp[x+1]))
+        for op in self.ops:
+            descs = []
+            descs.append(op.nodename())
+            g.node(str(op.idx), label='\n'.join(descs))
+            for succ in op.succ:
+                descs = []
+                descs.append(numpy.array2string(op.output, separator='x'))
+                g.edge(str(op.idx), str(succ), label='\n'.join(descs))
+
+        # input & output endpoints
+        last = self.ops[-1].idx
+        g.edge('input', '0', numpy.array2string(self.get_input(0, 0), separator='x'))
+        g.edge(str(last), 'output', numpy.array2string(self.get_output(last, 0), separator='x'))
+
+        if self.args.render:
+            g.render()
+        else:
+            g.view()
 
 class LoadConfig(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -46,75 +128,22 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def parse_model(args):
-    with open(args.model, 'rb') as fd:
-        return tflite.Model.GetRootAsModel(fd.read(), 0)
-    return None
-
-def get_successor(subgraph, op):
-    results = []
-    outputs = op.OutputsAsNumpy()
-    for i in range(subgraph.OperatorsLength()):
-        op = subgraph.Operators(i)
-        for x in op.InputsAsNumpy():
-            if x in outputs:
-                results.append(i)
-    return results
-
-def get_output(subgraph, op):
-    if op.OutputsLength():
-        tensor_index = op.Outputs(0)
-        tensor = subgraph.Tensors(tensor_index)
-        return tensor.ShapeAsNumpy()
-    return None
-
-def parse_subgraph(args, model):
-    results = []
-    subgraph = model.Subgraphs(0)
-    for i in range(subgraph.OperatorsLength()):
-        op = subgraph.Operators(i)
-        ctx = OpCtx(i, model.OperatorCodes(op.OpcodeIndex()))
-        ctx.succ = get_successor(subgraph, op)
-        ctx.output = get_output(subgraph, op)
-        results.append(ctx)
-    return results
-
-def do_plot(args, ctxs):
-    g = Digraph()
-    for ctx in ctxs:
-        if len(ctx.fus_grp) > 1:
-            with g.subgraph(name='cluster_'+str(ctx.fus_grp)) as c:
-                c.attr(color='blue')
-                c.attr(label=str(ctx.fus_grp))
-                c.edge_attr['style'] = 'invis'
-                for x in range(len(ctx.fus_grp) - 1):
-                    a = ctx.fus_grp[x]
-                    b = ctx.fus_grp[x+1]
-                    c.edge(ctxs[a].nodename(), ctxs[b].nodename())
-    for ctx in ctxs:
-        g.node(ctx.nodename())
-        for succ in ctx.succ:
-            g.edge(ctx.nodename(), ctxs[succ].nodename(), label=numpy.array2string(ctx.output, separator='x'))
-    g.view()
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', action=LoadConfig)
     parser.add_argument('-m', '--model', required=True)
     parser.add_argument('-g', '--graph', type=str2bool, nargs='?', const=True, default=False)
+    parser.add_argument('-r', '--render', type=str2bool, nargs='?', const=True, default=False)
     args = parser.parse_args()
 
-    model = parse_model(args)
-    ctxs = parse_subgraph(args, model)
+    mp = ModelParser(args)
+    mp.load_model()
+    mp.parse_subgraph()
 
-    for ctx in ctxs:
-        print(ctx)
-
-    ctxs[0].fus_grp = [0, 1, 2, 3]
-    ctxs[6].fus_grp = [6, 7, 8, 9]
+    mp.ops[0].fus_grp = [0, 1, 2, 3]
 
     if args.graph:
-        do_plot(args, ctxs)
+        mp.plot()
 
     return
 
